6d942a721f1d29b5c38a50dcfc56839a52e8191d
Giggs <Kevlarsol@gmail.com>
2022-04-27 16:50:26 -0700

    Ein card game foot in door

--- js/app.js
+++ js/app.js
@@ -1,7 +1,42 @@
-let count = 1;
+const computer = {
+    name: "Ein",
+    cards: [
 
-const doSomething = () => {
-    console.log("This did something in the console, " +count+" times.");
-    document.getElementById('jsDiv').innerHTML = "Something happened in the DOM "+count+" times. (also check the console)";
-    count++;
+    ]
 }
+
+const player = {
+    name: "Human",
+    cards: [
+        
+    ]
+}
+
+const pickCard = () =>{
+    return Math.floor(Math.random() * 10);
+}
+
+const selectCard = (cardNumber) => {
+    console.log(player.cards[cardNumber]);
+    document.getElementById("display").innerHTML= "<div class='cards'>"+player.cards[cardNumber]+"</div>"
+}
+
+const dealCards = (person) =>{
+    person.cards.push(pickCard());
+
+    let cardsHTML = ""
+
+    for (let i = 0; i < 3; i++) {
+        person.cards.push(pickCard());
+        console.log(i, person.cards, person.cards[i])
+        cardsHTML = cardsHTML+"<div class='cards' onClick='selectCard("+i+")'>"+person.cards[i]+"</div>"
+    }
+
+    document.getElementById("deck").innerHTML=cardsHTML;
+
+    return person.cards;
+}
+
+
+
+console.log("computer and player stats", dealCards(computer), dealCards(player))
